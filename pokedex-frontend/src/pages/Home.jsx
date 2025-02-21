7cbe4791c91108493756a9c738e17ff6b11a048e
Samy Hadji <s.hadji1906@gmail.com>
2025-02-21 10:01:11 +0100

    chore: clean up default Vite files and prepare frontend structure

---
+++ pokedex-frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Pokédex</h1>
+            <p>Bienvenue sur l'application Pokédex !</p>
+        </div>
+    );
+};
+
+export default Home;
